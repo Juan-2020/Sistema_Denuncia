27df5f18f61dbc3660c42f015e1497352aa9779c
Juan Alberto Godoy <63079197+Juan-2020@users.noreply.github.com>
2023-09-01 16:32:25 -0400

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,6 @@
+<div class="brand clearfix">
+		<a href="#" class="logo" style="font-size:16px;">Sistema|<b>Denuncia</b></a>
+		<span class="menu-btn"><i class="fa fa-bars"></i></span>
+		
+	</div>
+	
---
+++ includes/sidebar.php
@@ -0,0 +1,7 @@
+<nav class="ts-sidebar">
+<ul class="ts-sidebar-menu">			
+<li><a href="registration.php"><i class="fa fa-files-o"></i>Registro de Usuario</a></li>
+<li><a href="index.php"><i class="fa fa-user"></i> Acceso de Usuario</a></li>
+<li><a href="admin/index.php"><i class="fa fa-user"></i> Acceso de Administrador</a></li>
+</ul>
+</nav>
